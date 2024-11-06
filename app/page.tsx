511c7b4a3829942a24da340b7748a1beda8424f3
Ricardo Franco Delgado <rf041555@gmail.com>
2024-11-05 21:55:30 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
-  return (
-    <div className="flex items-center justify-center">
-      <h1 className="text-red-500 py-5">Hello World!</h1>
-    </div>
-  );
+  return <Button>Hello World</Button>;
 };
 
 export default Home;
