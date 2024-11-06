6c64158e983c296be82edb57d1333308d6dd1559
Ricardo Franco Delgado <rf041555@gmail.com>
2024-11-05 21:33:37 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return ( <h1>Hello World!</h1> );
-}
- 
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-red-500 py-5">Hello World!</h1>
+    </div>
+  );
+};
+
 export default Home;
