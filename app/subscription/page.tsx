6855282db02d7b99a82b657f0c69800c8aafeecb
Ricardo Franco Delgado <rf041555@gmail.com>
2024-11-12 17:07:16 -0300

    chore: remove signature mensaly the project is open source

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,20 +1,19 @@
-import { auth, clerkClient } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
-import AcquirePlanButton from "./_components/acquire-plan-button";
-import { Badge } from "../_components/ui/badge";
-import { getCurrentMonthTransactions } from "../_data/get-current-month-transactions";
+// import AcquirePlanButton from "./_components/acquire-plan-button";
+// import { Badge } from "../_components/ui/badge";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-  const user = await clerkClient().users.getUser(userId);
-  const currentMonthTransactions = await getCurrentMonthTransactions();
-  const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
+  // const user = await clerkClient().users.getUser(userId);
+  // const currentMonthTransactions = await getCurrentMonthTransactions();
+  // const hasPremiumPlan = user.publicMetadata.subscriptionPlan == "premium";
   return (
     <>
       <Navbar />
@@ -37,7 +36,7 @@ const SubscriptionPage = async () => {
               <div className="flex items-center gap-2">
                 <CheckIcon className="text-primary" />
                 <p>
-                  Apenas 10 transações por mês ({currentMonthTransactions}/10)
+                  {/* Apenas 10 transações por mês ({currentMonthTransactions}/10) */}
                 </p>
               </div>
               <div className="flex items-center gap-2">
@@ -49,11 +48,11 @@ const SubscriptionPage = async () => {
 
           <Card className="w-[450px]">
             <CardHeader className="relative border-b border-solid py-8">
-              {hasPremiumPlan && (
+              {/* {hasPremiumPlan && (
                 <Badge className="absolute left-4 top-12 bg-primary/10 text-primary">
                   Ativo
                 </Badge>
-              )}
+              )} */}
               <h2 className="text-center text-2xl font-semibold">
                 Plano Premium
               </h2>
@@ -72,7 +71,7 @@ const SubscriptionPage = async () => {
                 <CheckIcon className="text-primary" />
                 <p>Relatórios de IA</p>
               </div>
-              <AcquirePlanButton />
+              {/* <AcquirePlanButton /> */}
             </CardContent>
           </Card>
         </div>
